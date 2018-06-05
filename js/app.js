d207aaa087fbb2ea5be03b6c215719fe70e734fd
unknown <jeasleygtr@icloud.com>
2018-06-05 14:17:41 -0400

    working slides within modal

---
+++ js/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+$('.modal').modal();
+$('.slider').slider({full_width: true});
+});
