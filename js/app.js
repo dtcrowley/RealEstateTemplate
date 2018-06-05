8665c54cd984cb18efbaab0655011a2f60d33614
unknown <jeasleygtr@icloud.com>
2018-06-05 15:06:43 -0400

    fixed gallerytest3 functionality

--- js/app.js
+++ js/app.js
@@ -10,15 +10,29 @@
 //     indicators: true
 //   });
 
-$(document).ready(function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, {
-        'onOpenEnd': initCarouselModal
-    });
+// $(document).ready(function() {
+//     var elems = document.querySelectorAll('.modal');
+//     var instances = M.Modal.init(elems, {
+//         'onOpenEnd': initCarouselModal
+//     });
 
-    function initCarouselModal() {
-        var elems = document.querySelectorAll('.carousel');
-        var instances = M.Carousel.init(elems, {'fullWidth': true});
-        instances[0].set(2);
-    }
+//     function initCarouselModal() {
+//         var elems = document.querySelectorAll('.carousel');
+//         var instances = M.Carousel.init(elems, {'fullWidth': true});
+//         instances[0].set(2);
+//     }
+// });
+
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+$('.modal').modal();
+$('.slider').slider({
+  full_width: true,
+});
+$('.next').click(function() {
+  $('.slider').slider('next');
+});
+$('.prev').click(function() {
+  $('.slider').slider('prev');
+});
 });
