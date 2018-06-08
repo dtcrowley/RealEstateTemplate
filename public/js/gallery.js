2b89b9439e3f012116214fa8e52164e84b5325f7
unknown <jeasleygtr@icloud.com>
2018-06-08 13:06:59 -0400

    fixed modals on index, need styling

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,27 +1,3 @@
-// // var instance = M.Modal.getInstance(Modal);
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.modal');
-//     var instances = M.Modal.init(elems, 0.5);
-//   });
-
-//   var instance = M.Carousel.init({
-//     fullWidth: true,
-//     indicators: true
-//   });
-
-// $(document).ready(function() {
-//     var elems = document.querySelectorAll('.modal');
-//     var instances = M.Modal.init(elems, {
-//         'onOpenEnd': initCarouselModal
-//     });
-
-//     function initCarouselModal() {
-//         var elems = document.querySelectorAll('.carousel');
-//         var instances = M.Carousel.init(elems, {'fullWidth': true});
-//         instances[0].set(2);
-//     }
-// });
 
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
@@ -37,4 +13,4 @@ $(document).ready(function(){
   $('.prev').click(function() {
     $('.slider').slider('prev');
   });
-  });
+});
