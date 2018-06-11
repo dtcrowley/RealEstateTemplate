f6532d56c6f60eeeecc3f9938fd08b195761387e
dtcrowley <crowleyd1988@gmail.com>
2018-06-11 12:23:22 -0400

    added static modals on gallery page with slider on banner at top

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,16 +1,16 @@
 
 $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-  $('.modal').modal();
-  $('.slider').slider({
-    full_width: true,
-    indicators: false
-  });
-  
-  $('.next').click(function() {
-    $('.slider').slider('next');
-  });
-  $('.prev').click(function() {
-    $('.slider').slider('prev');
-  });
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+$('.modal').modal();
+$('.slider').slider({
+  full_width: true,
+  indicators: false
+});
+
+$('.next').click(function() {
+  $('.slider').slider('next');
+});
+$('.prev').click(function() {
+  $('.slider').slider('prev');
+});
 });
