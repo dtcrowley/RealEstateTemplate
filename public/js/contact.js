dc5ad899f9fd7145aef9237c36c3aa879c5a81db
dtcrowley <crowleyd1988@gmail.com>
2018-06-13 15:17:56 -0400

    Added new jquery to try to require form validation. almost working

--- public/js/contact.js
+++ public/js/contact.js
@@ -17,12 +17,32 @@ $(document).ready(function(){
     var name = "";
     var email = "";
     var comment = "";
-
-    // Capture Button Click
+    
     $("#add-to-firebase").on("click", function() {
         // Don't refresh the page!
         event.preventDefault();
-
+        $('form[name=registration').validate({
+            // Specify validation rules
+            rules: {
+                // The key name on the left side is the name attribute
+                // of an input field. Validation rules are defined
+                // on the right side
+                name: "required",
+                email: {
+                required: true,
+                // Specify that email should be validated
+                // by the built-in "email" rule
+                email: true
+                },
+                comment:"required"
+            },
+            // Specify validation error messages
+            messages: {
+                name: "Please enter your name",
+                email: "Please enter a valid email address",
+                comment:"Please enter your question or comment"
+            }
+        });
         //Providing data to Firebase
         name = $("#name-input").val().trim();
         email = $("#email-input").val().trim();
@@ -33,19 +53,22 @@ $(document).ready(function(){
             email: email,
             comment: comment,
         };
-
-        console.log(name, email, comment);
-
-        database.ref().push(userComment);
-
-        // var postSubmit = $("#form-wrapper").hide();
-        var thankYou = $("<h5>").text("Thanks for reaching out! We will respond to you as soon as possible.");
-        $("#thank-you-message").append(thankYou)
-        $("#thank-you-message").show();
-
-        name = $("#name-input").val("");
-        email = $("#email-input").val("");
-        comment = $("#comment-input").val("");
+            console.log(name, email, comment);
+    
+            database.ref().push(userComment);
+    
+            // var postSubmit = $("#form-wrapper").hide();
+            $("#thank-you-message").hide();
+            var thankYou = $("<h5>").text("Thanks for reaching out! We will respond to you as soon as possible.");
+            $("#thank-you-message").append(thankYou)
+            $("#thank-you-message").show();
+    
+            name = $("#name-input").val("");
+            email = $("#email-input").val("");
+            comment = $("#comment-input").val("");
+        });
+        $(".character-counter").hide();
     });
-    $(".character-counter").hide();
-});
+//     // Capture Button Click
+    
+// });
